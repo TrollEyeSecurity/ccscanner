a4f5f5a027775c4273fabcae5fdb78604bf6d622
Arozar Rozar <avery.rozar@critical-sec.com>
2020-08-17 13:00:47 -0400

    add SastImage

--- pkg/docker/constants.go
+++ pkg/docker/constants.go
@@ -2,6 +2,7 @@ package docker
 
 const MongoDbDockerImage = "docker.io/library/mongo:latest"
 const OwaspZapImage = "docker.io/owasp/zap2docker-stable"
+const SastImage = "docker.io/criticalsec/sast-scanner:latest"
 const NmapDockerImage = "docker.io/criticalsec/scanner:latest"
 const DnsReconImage = "docker.io/criticalsec/dnsrecon:latest"
 const OpenVasImage = "docker.io/criticalsec/openvas:latest"
