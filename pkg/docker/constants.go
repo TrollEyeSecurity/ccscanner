271ac1fb3041ff618220b6cacfd2945f6916291e
Arozar Rozar <avery.rozar@trolleyesecurity.com>
2022-02-08 23:23:44 -0500

    update SastImage to trolleye/sast-scanner:2021-Q3 and rename NmapDockerImage to KaliLinuxImage

--- pkg/docker/constants.go
+++ pkg/docker/constants.go
@@ -2,7 +2,7 @@ package docker
 
 const MongoDbDockerImage = "docker.io/library/mongo:4.4.10"
 const OwaspZapImage = "docker.io/owasp/zap2docker-stable"
-const SastImage = "docker.io/trolleye/sast-scanner:2021-Q3"
-const NmapDockerImage = "docker.io/trolleye/kali-linux:latest"
+const SastImage = "docker.io/trolleye/sast-scanner:2022-Q1"
+const KaliLinuxImage = "docker.io/trolleye/kali-linux:latest"
 const DnsReconImage = "docker.io/trolleye/dnsrecon:latest"
 const GVMImage = "docker.io/trolleye/gvm:20.8.2"
