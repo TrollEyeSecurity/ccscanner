e7f382bdd48c2da16a6c154ea52e24b6c5e71a61
Arozar Rozar <avery.rozar@trolleyesecurity.com>
2022-04-27 22:41:29 -0400

    add LoadDastConfiguration

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,28 @@ package config
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/TrollEyeSecurity/ccscanner/internal/database"
 	"os"
 )
 
 func LoadConfiguration(file string) Config {
 	var config Config
+	if file != "" {
+		configFile, err := os.Open(file)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		if configFile != nil {
+			jsonParser := json.NewDecoder(configFile)
+			jsonParser.Decode(&config)
+			configFile.Close()
+		}
+	}
+	return config
+}
+
+func LoadDastConfiguration(file string) database.DastConfig {
+	var config database.DastConfig
 	if file != "" {
 		configFile, err := os.Open(file)
 		if err != nil {
