4754b8d76def17ab95c959ef8aefa4de746bdbbe
Arozar Rozar <avery.rozar@trolleyesecurity.com>
2022-11-01 19:00:11 -0400

    update SastImage

--- pkg/docker/constants.go
+++ pkg/docker/constants.go
@@ -2,7 +2,7 @@ package docker
 
 const MongoDbDockerImage = "docker.io/library/mongo:latest"
 const OwaspZapImage = "docker.io/owasp/zap2docker-stable"
-const SastImage = "docker.io/trolleye/sast-scanner:2022-Q1"
+const SastImage = "docker.io/trolleye/sast-scanner:2022-Q3"
 const KaliLinuxImage = "docker.io/trolleye/kali-linux:latest"
 const DnsReconImage = "docker.io/trolleye/dnsrecon:latest"
 const NetReconImage = "docker.io/trolleye/netrecon:latest"
