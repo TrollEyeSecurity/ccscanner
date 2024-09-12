bc4ff4a20a5f7586a79a1a6c8223fe43333e7dc4
Avery Rozar <avery.rozar@trolleyesecurity.com>
2024-09-12 08:01:25 -0400

    update owasp zap

--- pkg/docker/constants.go
+++ pkg/docker/constants.go
@@ -1,6 +1,6 @@
 package docker
 
-const OwaspZapImage = "docker.io/owasp/zap2docker-stable"
+const OwaspZapImage = "ghcr.io/zaproxy/zaproxy:stable"
 const KaliLinuxImage = "docker.io/trolleye/kali-linux:latest"
 const NetReconImage = "docker.io/trolleye/netrecon:latest"
 const SnykCliImage = "docker.io/trolleye/snyk-cli:latest"
