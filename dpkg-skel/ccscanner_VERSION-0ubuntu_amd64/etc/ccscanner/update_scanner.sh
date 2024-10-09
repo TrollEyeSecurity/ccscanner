61622cde4655b41a18541418a107e55a324c22f0
Avery Rozar <avery.rozar@trolleyesecurity.com>
2024-10-09 10:13:32 -0400

    update the update_scanner.sh file for individual task managers

--- dpkg-skel/ccscanner_VERSION-0ubuntu_amd64/etc/ccscanner/update_scanner.sh
+++ dpkg-skel/ccscanner_VERSION-0ubuntu_amd64/etc/ccscanner/update_scanner.sh
@@ -29,13 +29,13 @@ done
 echo "sleeping for 60 secs"
 sleep 60
 
-sudo systemctl stop ccscanner cctaskmanager
+sudo systemctl stop ccscanner.service cc-dast-taskmanager.service cc-gvm-taskmanager.service cc-infrastructre-taskmanager.service cc-nmap-taskmanager.service cc-sast-taskmanager.service cc-url-taskmanager.service
 
 sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y
 
 docker_commands
 
-sudo systemctl enable --now ccscanner.service cctaskmanager.service
+sudo systemctl enable --now ccscanner.service cc-dast-taskmanager.service cc-gvm-taskmanager.service cc-infrastructre-taskmanager.service cc-nmap-taskmanager.service cc-sast-taskmanager.service cc-url-taskmanager.service
 
 cleanup_permissions
 
